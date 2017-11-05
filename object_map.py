21329adae1318a323c2488b2ab6e14a566c88b17
Misha Wagner <mishajw@gmail.com>
2017-11-05 16:22:52 +0000

    Add Road type and fix issues in parsing types

--- object_map.py
+++ object_map.py
@@ -1,4 +1,5 @@
-from typing import Union, List
+from enum import Enum
+from typing import Union, List, Dict
 import logging
 import osm_map
 
@@ -11,10 +12,15 @@ class Coords:
         self.lon = lon
 
     @classmethod
-    def from_osm(cls, osm: Union[osm_map.OsmNode, osm_map.OsmWay]):
-        assert "lat" in osm.attributes
-        assert "lon" in osm.attributes
-        return Coords(float(osm.attributes["lat"]), float(osm.attributes["lon"]))
+    def from_node(cls, node: osm_map.OsmNode):
+        assert "lat" in node.attributes
+        assert "lon" in node.attributes
+        return Coords(float(node.attributes["lat"]), float(node.attributes["lon"]))
+
+    @classmethod
+    def list_from_way(cls, way: osm_map.OsmWay):
+        # TODO this won't work
+        return [Coords.from_node(node) for node in way.nodes]
 
 
 class Item:
@@ -23,30 +29,57 @@ class Item:
 
 
 class NodeBasedItem(Item):
-    @classmethod
-    def from_node(cls, node: osm_map.OsmNode):
-        raise NotImplementedError()
-
-
-class WayBasedItem(Item):
-    @classmethod
-    def from_way(cls, node: osm_map.OsmWay):
-        raise NotImplementedError()
-
-
-class Tree(NodeBasedItem):
     def __init__(self, _id: int, coords: Coords):
         super().__init__(_id)
         self.coords = coords
 
     @classmethod
     def from_node(cls, node: osm_map.OsmNode):
+        raise NotImplementedError()
+
+
+class WayBasedItem(Item):
+    def __init__(self, _id: int, all_coords: List[Coords]):
+        super().__init__(_id)
+        self.all_coords = all_coords
+
+    @classmethod
+    def from_way(cls, way: osm_map.OsmWay):
+        raise NotImplementedError()
+
+
+class Tree(NodeBasedItem):
+    def __init__(self, _id: int, coords: Coords):
+        super().__init__(_id, coords)
+
+    @classmethod
+    def from_node(cls, node: osm_map.OsmNode):
         assert "natural" in node.attributes
         assert node.attributes["natural"] == "tree"
 
-        coords = Coords.from_osm(node)
+        coords = Coords.from_node(node)
 
         return Tree(node.id, coords)
+
+
+class Road(WayBasedItem):
+    class RoadType(Enum):
+        Residential = 0
+        Footway = 1
+
+    __road_type_dict: Dict[str, RoadType] = \
+        dict([(road_type.name.lower(), road_type) for road_type in RoadType])
+
+    def __init__(self, _id: int, all_coords: List[Coords], road_type: RoadType):
+        super().__init__(_id, all_coords)
+        self.road_type = road_type
+
+    @classmethod
+    def from_way(cls, way: osm_map.OsmWay):
+        assert "highway" in way.attributes
+        assert way.attributes["highway"] in Road.__road_type_dict.keys()
+
+        return Road(way.id, Coords.list_from_way(way), Road.__road_type_dict[way.attributes["highway"]])
 
 
 def __get_subclasses(cls):
@@ -63,22 +96,22 @@ def __get_subclasses(cls):
     return subclasses
 
 
-node_based_items = __get_subclasses(NodeBasedItem)
-way_based_items = __get_subclasses(WayBasedItem)
+node_based_creators = [subclass.from_node for subclass in __get_subclasses(NodeBasedItem)]
+way_based_creators = [subclass.from_way for subclass in __get_subclasses(WayBasedItem)]
 
 
 def parse(input_map: osm_map.OsmMap) -> List[Item]:
     def parse_list(l: Union[List[osm_map.OsmNode], List[osm_map.OsmWay]], classes) -> List[Item]:
         for element in l:
-            for element_class in classes:
+            for element_creator in classes:
                 try:
-                    log.debug(f"Trying to parse {element} as {element_class}")
-                    yield element_class.from_node(element)
+                    log.debug(f"Trying to parse {element} as {element_creator}")
+                    yield element_creator(element)
                     break
                 except AssertionError as e:
                     log.debug(f"Couldn't due to {e}")
 
-    node_items = parse_list(input_map.get_nodes(), node_based_items)
-    way_items = parse_list(input_map.get_ways(), node_based_items)
+    node_items = parse_list(input_map.get_nodes(), node_based_creators)
+    way_items = parse_list(input_map.get_ways(), way_based_creators)
 
     return list(node_items) + list(way_items)
